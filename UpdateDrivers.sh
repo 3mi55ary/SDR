b55fd88fa96d775e32c65dde5c68a715be3ab2cc
3mi55ary <Gapedwards4@gmail.com>
2025-09-18 00:00:41 -0400

    Create UpdateDrivers.sh

---
+++ UpdateDrivers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Updates to the latest SDR drivers.  Slightly modified from RTL-SDR's webpage.
+
+set -e # Exits on an error
+
+sudo apt purge -y ^librtlsdr
+sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr* /usr/local/include/rtl_* /usr/local/bin/rtl_*
+
+sudo apt-get install -y libusb-1.0-0-dev git cmake pkg-config
+
+git clone https://github.com/rtlsdrblog/rtl-sdr-blog
+cd rtl-sdr-blog
+mkdir build
+cd build
+cmake ../ -DINSTALL_UDEV_RULES=ON
+make
+sudo make install
+sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
+sudo ldconfig

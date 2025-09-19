870f186074e5c1d16d41be9f6d872e141643f533
3mi55ary <Gapedwards4@gmail.com>
2025-09-19 12:08:36 -0400

    Update UpdateDrivers.sh

--- UpdateDrivers.sh
+++ UpdateDrivers.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-# Updates to the latest SDR drivers.  Slightly modified from RTL-SDR's webpage.
+# Updates to the latest SDR drivers.  From RTL-SDR's webpage.
 
 set -e # Exits on an error
 
-sudo apt purge -y ^librtlsdr
+sudo apt purge ^librtlsdr
 sudo rm -rvf /usr/lib/librtlsdr* /usr/include/rtl-sdr* /usr/local/lib/librtlsdr* /usr/local/include/rtl-sdr* /usr/local/include/rtl_* /usr/local/bin/rtl_*
 
 sudo apt-get install -y libusb-1.0-0-dev git cmake pkg-config
-
-git clone https://github.com/rtlsdrblog/rtl-sdr-blog
+git clone https://github.com/osmocom/rtl-sdr
 cd rtl-sdr-blog
 mkdir build
 cd build
@@ -18,3 +17,7 @@ make
 sudo make install
 sudo cp ../rtl-sdr.rules /etc/udev/rules.d/
 sudo ldconfig
+
+echo 'blacklist dvb_usb_rtl28xxu' | sudo tee --append /etc/modprobe.d/blacklist-dvb_usb_rtl28xxu.conf
+
+echo '[+] Driver install successful.  Reboot System Now!'

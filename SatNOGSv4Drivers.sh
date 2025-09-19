421385c94f879224e27e29e39784539cd80eb8d4
3mi55ary <Gapedwards4@gmail.com>
2025-09-19 10:11:24 -0400

    Update SatNOGSv4Drivers.sh

--- SatNOGSv4Drivers.sh
+++ SatNOGSv4Drivers.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo apt install devscripts build-essential cmake libusb-1.0-0-dev
 dget -q -u https://download.opensuse.org/repositories/home:/knegge:/branches:/home:/librespace:/satnogs-unstable/Debian_11/rtl-sdr_2.0.2-3satnogs1.dsc
 cd rtl-sdr-2.0.2

5ab5fbe43492f7da1b8cebda5e7ae9e1c107e2c2
3mi55ary <Gapedwards4@gmail.com>
2025-09-20 16:40:36 -0400

    Update RTLSDRv4Perms.sh

--- RTLSDRv4Perms.sh
+++ RTLSDRv4Perms.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Used to allow the SatNOGS Client Docker container to access the RTL-SDR v4.
 
 echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="2838", MODE:="0666"' \
 | sudo tee /etc/udev/rules.d/99-rtlsdr.rules

c87c8a08dc6cbb972e5378c85a4bc786dee57be9
3mi55ary <Gapedwards4@gmail.com>
2025-09-20 16:39:36 -0400

    Create RTLSDRv4Perms.sh

---
+++ RTLSDRv4Perms.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="0bda", ATTRS{idProduct}=="2838", MODE:="0666"' \
+| sudo tee /etc/udev/rules.d/99-rtlsdr.rules
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
